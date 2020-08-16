36ff16fdbe95866ff94a0b5458fc9f08de88047a
Brad Cassity <bradley.cassity@gmail.com>
2020-08-16 11:25:45 -0600

    Added generatePassword function and included some initial variables and arrays

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// Assignment code here
+// Generate Password Function
+var generatePassword = function() {
+  // establishing allowed character arrays
+  const lowerCaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const uppderCaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  const numericValues = [0,1,2,3,4,5,6,7,8,9];
+  const specialChars = ['!','"','#','$','&','%','^',"'",'(',')','*','+','-','.','<','>','?','/','[',']','{','}','|','~','_'];
+  var includedCharacters = [];
+  var generatedPass = '';
+  var passLength = 0;
+  
+  
+  // return the generated password
+  return generatedPass;
+}
 
 
 // Get references to the #generate element
